ccce0da136a42dff04e09dba272b3b4f78008053
Jayesh <useradd@DESKTOP-1E84FKM.localdomain>
2022-11-08 21:15:16 +0530

    Add pre-commit hook

---
+++ __tests__/todo.js
@@ -0,0 +1,16 @@
+//const { describe } = require('eslint/lib/rule-tester/rule-tester');
+const todoList = require('../todo');
+const {all, markAsComplete, add} = todoList();
+describe("TodoList Test Suite", () => {
+    test("Should add new todo", () => {
+        expect(all.length).toBe(0);
+        add(
+            {
+                title: "Test todo",
+                completed: false,
+                dueDate : new Date().toLocaleDateString("en-CA")
+            }
+        );
+        expect(all.length).toBe(1);
+    });
+});
